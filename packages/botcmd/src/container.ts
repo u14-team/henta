200af80960e138cfa51f741c297a8446cd300106
Daniil Koshkin <admin@nyashmyash99.ru>
2024-03-12 18:20:28 +0300

    Refactor find command method

--- packages/botcmd/src/container.ts
+++ packages/botcmd/src/container.ts
@@ -24,12 +24,36 @@ export default class BotcmdContainer {
     commandLine: string,
   ): { pattern: string; command: IBuildedCommand } | null {
     // maybe faster?
-    for (const [key, value] of this.commandByName) {
-      if (key === commandLine || `${commandLine} `.startsWith(key)) {
-        return { pattern: key, command: value };
+    const preparedCommand = commandLine.split(' ');
+
+    let maxMatches = -1;
+    let closestArray: string[] = null;
+    let closestCommand: ReturnType<BotcmdContainer['find']> = null;
+
+    for (const [key, command] of this.commandByName) {
+      const preparedKey = key.split(' ');
+      // Without set, sub-commands containing a similar string to parent command will always override.
+      // This solution causes a bug, but it's a better way.
+      const inclusion = new Set(
+        preparedKey.filter((item) => preparedCommand.includes(item)),
+      );
+
+      const matches = inclusion.size;
+      if (!matches) {
+        continue;
+      }
+
+      if (
+        matches > maxMatches ||
+        // Prefer more basic commands.
+        (matches === maxMatches && closestArray?.length > preparedKey.length)
+      ) {
+        maxMatches = matches;
+        closestArray = preparedKey;
+        closestCommand = { pattern: key, command };
       }
     }
 
-    return null;
+    return closestCommand;
   }
 }
