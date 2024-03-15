5cc5d0de590cb92e91bee0e1bb17ff666731ed59
Daniil Koshkin <admin@nyashmyash99.ru>
2024-03-15 13:41:46 +0300

    Simplify solution

--- packages/botcmd/src/container.ts
+++ packages/botcmd/src/container.ts
@@ -15,45 +15,19 @@ export default class BotcmdContainer {
       });
     }
 
-    this.commandByName = new Map(
-      [...this.commandByName].sort(([a], [b]) => b.length - a.length),
-    );
+    this.commandByName = new Map([...this.commandByName].sort());
   }
 
   public find(
     commandLine: string,
   ): { pattern: string; command: IBuildedCommand } | null {
     // maybe faster?
-    const preparedCommand = commandLine.split(' ');
-
-    let maxMatches = -1;
-    let closestArray: string[] = null;
-    let closestCommand: ReturnType<BotcmdContainer['find']> = null;
-
-    for (const [key, command] of this.commandByName) {
-      const preparedKey = key.split(' ');
-      // Without set, sub-commands containing a similar string to parent command will always override.
-      // This solution causes a bug, but it's a better way.
-      const inclusion = new Set(
-        preparedKey.filter((item) => preparedCommand.includes(item)),
-      );
-
-      const matches = inclusion.size;
-      if (!matches) {
-        continue;
-      }
-
-      if (
-        matches > maxMatches ||
-        // Prefer more basic commands.
-        (matches === maxMatches && closestArray?.length > preparedKey.length)
-      ) {
-        maxMatches = matches;
-        closestArray = preparedKey;
-        closestCommand = { pattern: key, command };
+    for (const [key, value] of this.commandByName) {
+      if (key === commandLine || commandLine.startsWith(`${key} `)) {
+        return { pattern: key, command: value };
       }
     }
 
-    return closestCommand;
+    return null;
   }
 }
